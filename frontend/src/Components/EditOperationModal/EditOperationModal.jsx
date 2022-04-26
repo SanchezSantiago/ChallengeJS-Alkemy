73d9566da7886c5d09117d94c94110f15396ea4a
Santiago Sanchez Nieva <santiagosancheznieva@gmail.com>
2022-04-25 22:46:02 -0300

    Fix: EditOperationModal
    It has been possible to fix the bug that only allowed an edition if the date was changed again.

--- frontend/src/Components/EditOperationModal/EditOperationModal.jsx
+++ frontend/src/Components/EditOperationModal/EditOperationModal.jsx
@@ -15,10 +15,7 @@ const EditOperationModal = (props) => {
       amount: props.operationInfo.amount,
       date: props.operationInfo.date,
     });
-    form.setFieldsValue({ //This will put a default value in every input of the form
-      concept: props.operationInfo.concept,
-      amount: props.operationInfo.amount,
-    });
+
     setIsModalVisible(true);
   };
 
@@ -36,16 +33,22 @@ const EditOperationModal = (props) => {
   
 
   const handleSubmit = async () =>{ //Put data to the database
-    try{
+      operationData.date = moment(operationData.date).utc().format('YYYY/MM/DD') //if they didn't change the date, this will be put into a format that SQL can accept
        await Axios.put(`http://localhost:3001/api/operations/${props.operationInfo.id}`, operationData);
        message.success('Operation edited successfully!');
+       console.log(operationData)
       handleCancel();
-    } catch (err){
-      console.log(err)
-    }
-    
-    
   };
+  const fail = () =>{
+    message.error('Edition failed, there are fields that are required!');
+  }
+  const dateFormat = "YYYY/MM/DD";
+
+const initialValues = {
+  concept: operationData.concept,
+  amount: operationData.amount,
+  date: moment(operationData.date)
+};
 
   const handleDate = (date,dateString) => {
     operationData.date = dateString; //Set the date of the operation
@@ -58,10 +61,13 @@ const EditOperationModal = (props) => {
       </Button>
       <Modal title="Edit" visible={isModalVisible} onCancel={handleCancel} footer={null}>
       <Form
-      name="basic"
-      initialValues={{ remember: true }}
-      autoComplete="off"
       form={form}
+      name="basic"
+      initialValues={initialValues}
+      autoComplete="off"
+      onFinish={handleSubmit}
+      onFinishFailed={fail}
+
     >
       <Form.Item
         label="Concept"
@@ -94,13 +100,13 @@ const EditOperationModal = (props) => {
         rules={[{ required: true, message: 'Please input the date!' }]}
       >
         <DatePicker 
+        format={dateFormat} 
         onChange={handleDate}
-        placeholder={moment(operationData.date).utc().format('YYYY/MM/DD')}//For some reason it crashes when i use "default Value"... Anymays
         size='large'
         />
       </Form.Item>
       <Form.Item >
-        <Button type="primary" onClick={handleSubmit} onSubmit={handleSubmit}>
+        <Button type="primary" htmlType='submit'>
           Submit
         </Button>
       </Form.Item>
